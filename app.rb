d44e4394e29fd28ae17cb0c6e4159500487c5eed
Daniel Childs <dc.1994@outlook.com>
2018-04-23 15:22:17 +0100

    create link in artist.erb save changes before large re-structure

--- app.rb
+++ app.rb
@@ -10,18 +10,26 @@ get "/home" do
   erb(:home)
 end
 
-get "/home/:stock" do
-  # @artists = Artist.all()
+get "/home/stock" do
   @albums = Album.show_all()
   erb(:stock)
 end
+
+
 
 #new
 
 
 #show
-get "/stock/:artist" do
-  "Display artists"
+get "/home/artist" do
+  @artists = Artist.show_all()
+  erb(:artist)
+end
+
+get "/home/artist/:id" do
+  @artist = Artist.find_by_id(params["id"].to_i)
+  @albums = Album.show_all()
+  erb(:show_albums)
 end
 
 
